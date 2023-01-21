670b895c7a2bf571e12c838a297d21ce1b4ff29d
jousef1 <yousefel560@gmail.com>
2023-01-21 15:47:49 +0800

    creating adding item functionality

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+
 // Get the input element and the buttons
 const quantityInput = document.getElementById("quantity");
 const decreaseButton = document.getElementById("decrease-quantity");
@@ -22,251 +23,150 @@ increaseButton.addEventListener("click", function() {
   }
 });
 
+//
+
+const addButton = document.getElementById("add-button");
+addButton.addEventListener("click", function() {
+  // Get the selected item and quantity
+  const selectedItem = document.getElementById("items").value;
+  const selectedQuantity = document.getElementById("quantity").value;
+
+  // Create a new div to hold the added item
+  const newItemDiv = document.createElement("div");
+  newItemDiv.classList.add("added-item");
+
+  // Create a div for the item image
+  const itemImageDiv = document.createElement("div");
+  itemImageDiv.classList.add("item_image");
+  // Create an image element
+  const itemImage = document.createElement("img");
+  itemImage.setAttribute("src", "https://picsum.photos/200/300");
+  itemImageDiv.appendChild(itemImage);
+
+  // Create a div for the item name and price
+  const itemInfoDiv = document.createElement("div");
+  itemInfoDiv.classList.add("item_info");
+  // Create a div for the item name
+  const itemNameDiv = document.createElement("div");
+  itemNameDiv.classList.add("item_name");
+  itemNameDiv.innerText = selectedItem;
+  // Create a div for the item price
+  const itemPriceDiv = document.createElement("div");
+  itemPriceDiv.classList.add("item_price");
+  // Create an input field for the price and set its value to 0
+  const itemPrice = document.createElement("input");
+  itemPrice.value = "$0";
+  itemPriceDiv.innerText = "$";
+
+  itemPrice.setAttribute("type", "number");
+  itemPrice.classList.add("price-input");
+  itemPriceDiv.appendChild(itemPrice);
+  itemInfoDiv.appendChild(itemNameDiv);
+  itemInfoDiv.appendChild(itemPriceDiv);
+
+  // Create a div for the item quantity
+  const itemQuantityDiv = document.createElement("div");
+  itemQuantityDiv.classList.add("item_quantity");
+  // Create an input field for the quantity and set its value to the selected quantity
+  const itemQuantity = document.createElement("input");
+  itemQuantity.value = selectedQuantity;
+  itemQuantity.setAttribute("type", "number");
+  itemQuantity.setAttribute("min", "1");
+  itemQuantity.setAttribute("max", "100");
+  itemQuantity.classList.add("quantity-input");
+  itemQuantityDiv.appendChild(itemQuantity);
+
+  // Create a decrease button
+  const decreaseButton = document.createElement("button");
+  decreaseButton.innerText = "-";
+  decreaseButton.addEventListener("click", function() {
+    // Decrement the quantity by 1
+    let quantity = parseInt(itemQuantity.value);
+    if (quantity > 1) {
+      quantity--;
+      itemQuantity.value = quantity;
+    }
+  });
+
+  // Create an increase button
+  const increaseButton = document.createElement("button");
+  increaseButton.innerText = "+";
+  increaseButton.addEventListener("click", function() {
+    // Increment the quantity by 1
+    let quantity = parseInt(itemQuantity.value);
+    if (quantity < 100) {
+      quantity++;
+      itemQuantity.value = quantity;
+    }
+  });
+  itemQuantityDiv.appendChild(decreaseButton);
+  itemQuantityDiv.appendChild(increaseButton);
+
+  // Create a div for the checkbox and delete button
+  const itemActionsDiv = document.createElement("div");
+  // Create a checkbox
+  const itemCheckbox = document.createElement("input");
+  itemCheckbox.setAttribute("type", "checkbox");
+  itemCheckbox.classList.add("item-checkbox");
+  // Create a delete button
+  const deleteButton = document.createElement("button");
+  deleteButton.innerText = "Delete";
+  deleteButton.addEventListener("click", function() {
+    newItemDiv.remove();
+  });
+  itemActionsDiv.appendChild(deleteButton);
+  itemActionsDiv.appendChild(itemCheckbox);
+
+  // Append all the item's components to the main div
+  newItemDiv.appendChild(itemImageDiv);
+  newItemDiv.appendChild(itemInfoDiv);
+  // 
+
+  // 
+  newItemDiv.appendChild(itemQuantityDiv);
+  newItemDiv.appendChild(itemActionsDiv);
+
+  // Append the new div to the "addeditems" div
+  const addedItemsDiv = document.querySelector(".addeditems");
+  addedItemsDiv.appendChild(newItemDiv);
+
+  // Reset the select and quantity input values
+  document.getElementById("items").value = "Select an item";
+  document.getElementById("quantity").value = 1;
+  document.getElementById("price").value = 0;
+});
+
+// Create a div to hold the total price
+const totalPriceDiv = document.createElement("div");
+totalPriceDiv.classList.add("total_price");
+
+// Create a div for the total
+const totalDiv = document.createElement("div");
+totalDiv.classList.add("total");
+totalDiv.innerText = "total: $";
+
+// Create an input field for the total and set its value to 0
+const total = document.createElement("input");
+total.value = 0;
+total.setAttribute("type", "text");
+total.classList.add("total-input");
+totalDiv.appendChild(total);
+
+// Create a save button
+const saveButton = document.createElement("button");
+saveButton.innerText = "save";
+saveButton.addEventListener("click", function() {
+  // Calculate the total by multiplying the price by the quantity
+  // total.value = itemPrice.value * itemQuantity.value;
+  total.value = parseFloat(itemPrice.value) * parseFloat(itemQuantity.value);
+
+});
+
+// Append the total and save button to the total price div
+totalPriceDiv.appendChild(totalDiv);
+totalPriceDiv.appendChild(saveButton);
+
+// Append the total price div to the new item div
+newItemDiv.appendChild(totalPriceDiv);
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// var items = [
-//   { "Id": 1, "name": "Item 1", "price": 10.99, "Image": "https://example.com/item1.jpg" },
-//   { "Id": 2, "name": "Item 2", "price": 15.99, "Image": "https://example.com/item2.jpg" },
-//   { "Id": 3, "name": "Item 3", "price": 20.99, "Image": "https://example.com/item3.jpg" },
-//   { "Id": 4, "name": "Item 4", "price": 25.99, "Image": "https://example.com/item4.jpg" },
-//   { "Id": 5, "name": "Item 5", "price": 30.99, "Image": "https://example.com/item5.jpg" },
-//   { "Id": 6, "name": "Item 6", "price": 35.99, "Image": "https://example.com/item6.jpg" },
-//   { "Id": 7, "name": "Item 7", "price": 40.99, "Image": "https://example.com/item7.jpg" },
-//   { "Id": 8, "name": "Item 8", "price": 45.99, "Image": "https://example.com/item8.jpg" },
-//   { "Id": 9, "name": "Item 9", "price": 50.99, "Image": "https://example.com/item9.jpg" },
-//   { "Id": 10, "name": "Item 10", "price": 55.99, "Image": "https://example.com/item10.jpg" }
-// ];
-
-// for (let i = 0; i < items.length; i++) {
-//   var option = document.createElement("option");
-//   option.value = items[i].Id;
-//   option.dataset.name = items[i].name;
-//   option.dataset.price = items[i].price;
-//   option.dataset.img = items[i].Image;
-//   option.text = items[i].name;
-//   document.getElementById("item-select").appendChild(option);
-// }
-
-// let totalPrice = 0;
-
-// // function addSection() {
-// //   var select = document.getElementById("item-select");
-// //   var selectedOption = select.options[select.selectedIndex];
-// //   var quantity = document.getElementById("quantity").textContent;
-// //   var price = document.getElementById("price").value; //get the value of the input field
-// //   var selectedItem = {
-// //     id: selectedOption.value,
-// //     name: selectedOption.dataset.name,
-// //     price: price,
-// //     image: selectedOption.dataset.img,
-// //     quantity: quantity
-// //   };
-
-// //   if (selectedItem.id) {
-// //     var newSection = document.createElement("div");
-// //     newSection.innerHTML = `
-// //       <p>Name: ${selectedItem.name}</p>
-// //       <p>Price: <input type="text" id="price" 
-// //       <p class="quantity_counter">Quantity: ${selectedItem.quantity}</p>
-// //       <img src="${selectedItem.image}">
-// //       <button onclick="deleteSection(this.parentNode)">Delete</button>
-
-// //     `;
-// //     // document.body.appendChild(newSection);
-// //     document.getElementById("container").appendChild(newSection);
-// //     document.getElementById("quantity").textContent = 1;
-// //     totalPrice += parseFloat(selectedItem.price) * parseInt(quantity);
-// //     // totalPrice += (parseFloat(selectedItem.price) * parseInt(quantity)).toFixed(2);
-// //     document.getElementById("total-price").textContent = `Total Price: ${totalPrice}`;
-// //   }
-// //   totalPrice += parseFloat(price) * parseInt(quantity);
-// //   document.getElementById("item-select").options[0].text = "Select an item";
-// //   document.getElementById("item-select").value = "";
-// // }
-
-// function addSection() {
-//   var select = document.getElementById("item-select");
-//   var selectedOption = select.options[select.selectedIndex];
-//   var quantity = document.getElementById("quantity").textContent;
-//   var selectedItem = {
-//     id: selectedOption.value,
-//     name: selectedOption.dataset.name,
-//     image: selectedOption.dataset.img,
-//     quantity: quantity
-//   };
-
-//   if (selectedItem.id) {
-//     var newSection = document.createElement("div");
-//     newSection.innerHTML = `
-//       <p>Name: ${selectedItem.name}</p>
-//       <p>Price: <input type="text" id="price" value="${selectedOption.dataset.price}"></p>
-//       <p class="quantity_counter">Quantity: ${selectedItem.quantity}</p>
-//       <img src="${selectedItem.image}">
-//       <button onclick="deleteSection(this.parentNode)">Delete</button>
-
-//     `;
-//     document.getElementById("container").appendChild(newSection);
-//     document.getElementById("quantity").textContent = 1;
-//     var price = document.getElementById("price").value;
-//     totalPrice += parseFloat(price) * parseInt(quantity);
-//     document.getElementById("total-price").textContent = `Total Price: ${totalPrice}`;
-//   }
-//   document.getElementById("item-select").options[0].text = "Select an item";
-//   document.getElementById("item-select").value = "";
-// }
-
-
-// function deleteSection(section) {
-//   var deletedPrice = parseFloat(section.getElementsByTagName("input")[0].value);
-//   var deletedQuantity = parseInt(section.getElementsByClassName("quantity_counter")[0].textContent.split(" ")[1]);
-//   totalPrice -= deletedPrice * deletedQuantity;
-//   document.getElementById("total-price").textContent = `Total Price: ${totalPrice}`;
-//   document.getElementById("container").removeChild(section);
-//   if(!document.getElementById("container").hasChildNodes()){
-//     totalPrice = 0;
-//     document.getElementById("total-price").textContent = `Total Price: ${totalPrice}`;
-//   }
-// }
-
-
-// function increaseQuantity() {
-//   var quantityElement = document.getElementById("quantity");
-//   var quantity = parseInt(quantityElement.textContent);
-//   quantity++;
-//   quantityElement.textContent = quantity;
-// }
-
-// function decreaseQuantity() {
-//   var quantityElement = document.getElementById("quantity");
-//   var quantity = parseInt(quantityElement.textContent);
-//   if(quantity>1){
-//     quantity--;
-//     quantityElement.textContent = quantity;
-//   }
-// }
-
-// window.onload = function() {
-//   if(totalPrice) {
-//     document.getElementById("total-price").textContent = `Total Price: ${totalPrice}`;
-//   } else {
-//     document.getElementById("total-price").textContent = "Total Price: 0";
-//   }
-// }
-
-
-
-// document.getElementById("price").addEventListener("input", function(){
-//   totalPrice += parseFloat(this.value) * parseInt(quantity);
-// });
